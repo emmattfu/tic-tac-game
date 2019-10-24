9d2ef1f87317c72077c4b3d8167d970c8008f8c2
emmattfu <makcimdjan@gmail.com>
2019-10-24 14:50:32 +0300

    draw

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
-      count: 0
+      count: 0,
+      result: ''
     };
 
     this.winnerLine = [
@@ -47,21 +48,34 @@ class App extends React.Component {
 
   isWinner = (winnerTeam) => {
     for (let i = 0; i < this.winnerLine.length; i++) {
-      let line = this.winnerLine[i]
+      let line = this.winnerLine[i];
+      if (this.state.squares[line[0]] === winnerTeam && this.state.squares[line[1]] === winnerTeam && this.state.squares[line[2]] === winnerTeam) {
+        this.setState({result: winnerTeam})
+      }
     }
+
+    setTimeout(() => {
+      this.setState({squares: Array(9).fill(null)});
+      this.setState({count: 0});
+      this.setState({result: ''});
+     }, 10000);
   }
 
   isDraw() {
    if (this.state.squares.every(el => el !== null)) {
      alert('Ничья!');
      setTimeout(() => {
-      this.setState({squares: Array(9).fill(null)})
+      this.setState({squares: Array(9).fill(null)});
+      this.setState({count: 0});
+      this.setState({result: ''});
      }, 3000);
    }
   }
 
   render() {
-    
+    let result = '';
+    this.state.result === '' ? result = '' : result = <div className="result">{this.state.result} won!</div>
+
     return (
       <div className="tic-tac">
         <div className="square" onClick={this.clickHandler} data="0">{this.state.squares[0]}</div>
@@ -73,6 +87,7 @@ class App extends React.Component {
         <div className="square" onClick={this.clickHandler} data="6">{this.state.squares[6]}</div>
         <div className="square" onClick={this.clickHandler} data="7">{this.state.squares[7]}</div>
         <div className="square" onClick={this.clickHandler} data="8">{this.state.squares[8]}</div>
+        {result}
       </div>
     );
   }
